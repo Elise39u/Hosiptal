99c622411f9de037d6efd3130673250bd60897b0
Justin van de laar <justin555@live.nl>
2017-03-15 11:09:41 +0100

    Added the changed database

--- hospital.sql
+++ hospital.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Machine: 127.0.0.1
--- Gegenereerd op: 25 mrt 2015 om 01:41
--- Serverversie: 5.6.17
--- PHP-versie: 5.5.12
+-- Host: localhost
+-- Gegenereerd op: 15 mrt 2017 om 10:06
+-- Serverversie: 5.7.9
+-- PHP-versie: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,38 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databank: `hospital`
+-- Database: `hospital`
 --
-CREATE DATABASE IF NOT EXISTS `hospital` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `hospital`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(1024) NOT NULL,
+  `adress` varchar(1024) NOT NULL,
+  `age` int(11) NOT NULL,
+  `email` varchar(1024) NOT NULL,
+  `pet_name` varchar(1024) NOT NULL,
+  `cheked_in` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cheked_out` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `client`
+--
+
+INSERT INTO `client` (`id`, `name`, `adress`, `age`, `email`, `pet_name`, `cheked_in`, `cheked_out`) VALUES
+(4, 'Jan willem', 'Mollenburgse weg', 32, 'unknow@hotmail.com', 'Kitten', '2017-03-14 00:16:14', NULL),
+(2, 'Kirsten', 'Klaverstraat 24 a', 43, 'Kirsten@live.nl', 'Jazzzz', '2017-03-13 09:46:42', NULL);
 
 -- --------------------------------------------------------
 
@@ -34,17 +59,46 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `name` varchar(50) DEFAULT NULL,
   `species` varchar(50) DEFAULT NULL,
   `status` text,
+  `gender` enum('Male','Female','Unkown','') DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `patient`
 --
 
-INSERT INTO `patient` (`id`, `name`, `species`, `status`) VALUES
-(1, 'Bobbie', 'hond', 'Koorts, eet slecht, blaft veel te veel'),
-(2, 'Minoes', 'kat', 'Drinkt niet, haaruitval, mager'),
-(3, 'Kees', 'hond', 'Eet te veel, vetzucht, jankt en kotst');
+INSERT INTO `patient` (`id`, `name`, `species`, `status`, `gender`) VALUES
+(5, 'Daisy', 'turtle', 'Cry all the time and has too much energie', 'Male'),
+(6, 'Kittie', 'Cat', 'Won`t eat and cry every time', 'Female'),
+(7, 'Jazzz', 'Kitten', 'Wont eat and drink', 'Unkown'),
+(8, 'IEDERE DAG', 'Kitten', 'Stabiel', 'Male'),
+(9, 'Siep', 'hond', 'Wil niet eten en Jankt', 'Unkown'),
+(10, 'Hond', 'Cat', 'Wont sleep', 'Unkown');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `species`
+--
+
+DROP TABLE IF EXISTS `species`;
+CREATE TABLE IF NOT EXISTS `species` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sort` varchar(1024) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `species`
+--
+
+INSERT INTO `species` (`id`, `sort`) VALUES
+(1, 'Dog'),
+(2, 'Cat'),
+(3, 'turtle'),
+(4, 'Puppy'),
+(6, 'Bird'),
+(8, 'Camel');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
